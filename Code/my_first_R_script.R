b62b6f11bf6eceef3bdebe4a7b7b01c1fba579c1
AndreaPetrone <iis.petrone.andrea@gmail.com>
2024-10-03 15:45:43 +0200

    Update my_first_R_script.R

--- Code/my_first_R_script.R
+++ Code/my_first_R_script.R
@@ -6,3 +6,13 @@ maria <- 4 + 2
 maria
 cato + maria 
 cato^maria
+
+# arrays: all vectors are a series of data
+andrea <- c(10, 15, 20, 50, 70) # we need to concatenate them with a function, an operation, the function is "c"
+
+sofia <- c(100, 200, 300, 400, 500)
+plot(sofia, andrea)
+plot(sofia, andrea, pch=19)
+plot(sofia, andrea, pch = 19, cex = 2)
+plot(sofia, andrea, pch = 19, cex = 2, col = "blue")
+plot(sofia, andrea, pch = 19, cex = 2, col = "blue", xlab = "CO2", ylab = "amount of fruits")
