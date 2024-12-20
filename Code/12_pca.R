650901b2aafc56b680dc874cf99c98d70ca41dac
AndreaPetrone <iis.petrone.andrea@gmail.com>
2024-12-20 10:09:51 +0100

    Update 12_pca.R

--- Code/12_pca.R
+++ Code/12_pca.R
@@ -1,33 +1,3 @@
-# FIRST TYPE OF NOTES
-
-# PCA
-# see picture taken of the blackboard
-
-library(imageRy)
-library(terra)
-im.list()
-
-sent <- im.import("sentinel.png")
-
-pairs(sent) 
-
-# to see the correlation amongst all of the bands (of sentinel image)
-# we can call these systems and compact them in one pca
-
-sentpc <- im.pca(sent) 
-
-# there is the variability explained by the principal component
-
-plot(sentpc) # because the sentpc is the principal component
-
-# the first pc is that with highest variability (might explain 80% of the variability)
-sentpc
-pc1 <- sentpc[[1]]
-pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
-plot(pc1sd)
-
-# SECOND TYPE
-#12/12 
 #PCA 
 
 library(imageRy)
@@ -39,26 +9,31 @@ im.list()
 sent <- im.import("sentinel.png")
 
 pairs(sent)
+# to see the correlation amongst all of the bands (of sentinel image) in a plot 
+# we can call these systems and compact them in one pca
 
-# perform PCA on sent
+# perform PCA on sent using function im.pca (keeps the variability reducing the information)
 sentpc <- im.pca(sent) 
 sentpc
+plot(sentpc) # the deviation standard is on the image
 
-pc1 <- sentpc[[1]]
-pc1
+# there is the variability explained by the principal component in the pc1, we extract pc1 (more variability = more information)
 
-pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
-plot(pc1sd)
+pc1 <- sentpc[[1]] # 80% of variability 
+pc1 # you see useful information like coordinates
 
-viridisc <- colorRampPalette(viridis(7))(255)
-plot(pc1, col=viridisc)
+pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd) # function focal to calculate standard deviation and shows areas with more variability
+plot(pc1sd) 
+
+viridisc <- colorRampPalette(viridis(7))(255) # for colorblind people viridis 
+plot(pc1, col=viridisc) 
 
 
 # calculating standard deviation ontop of pc1
 pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
 plot(pc1sd3, col=viridisc)
 
-pc1sd7 <- focal(pc1, matrix(1/49, 7, 7), fun=sd)
+pc1sd7 <- focal(pc1, matrix(1/49, 7, 7), fun=sd) # different dimension to calculate sd
 plot(pc1sd7, col=viridisc)
 
 par(mfrow=c(2,3))
