06dcbb1de853a63fd661916487d9979ca2da1a5e
AndreaPetrone <iis.petrone.andrea@gmail.com>
2025-02-05 16:45:58 +0100

    Update 12_pca.R

--- Code/12_pca.R
+++ Code/12_pca.R
@@ -1,4 +1,4 @@
-#PCA 
+# PCA 
 
 library(imageRy)
 library(terra)
@@ -10,26 +10,28 @@ sent <- im.import("sentinel.png")
 
 pairs(sent)
 # to see the correlation amongst all of the bands (of sentinel image) in a plot 
-# we can call these systems and compact them in one pca
 
-# perform PCA on sent using function im.pca (keeps the variability reducing the information)
+# perform PCA on sent using function im.pca 
+# keeps the variability reducing the information
+
 sentpc <- im.pca(sent) 
 sentpc
-plot(sentpc) # the deviation standard is on the image
+plot(sentpc) 
 
-# there is the variability explained by the principal component in the pc1, we extract pc1 (more variability = more information)
+# there is the variability explained by the principal component in the pc1
+# we extract pc1 (more variability = more information)
 
 pc1 <- sentpc[[1]] # 88,59% of variability 
-pc1 # you see useful information like coordinates
+pc1 
 
-pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd) # function focal to calculate standard deviation and shows areas with more variability
+pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd) # focal to calculate standard deviation and shows areas with more variability
 plot(pc1sd) 
 
 viridisc <- colorRampPalette(viridis(7))(255) # for colorblind people viridis 
 plot(pc1, col=viridisc) 
 
 
-# calculating standard deviation ontop of pc1
+# calculating standard deviation on top of pc1
 pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
 plot(pc1sd3, col=viridisc)
 
@@ -38,7 +40,7 @@ plot(pc1sd7, col=viridisc)
 
 par(mfrow=c(2,3))
 im.plotRGB(sent, 2, 1, 3)
-# sd from the variability script:
+
 plot(sd3, col=viridisc)
 plot(sd7, col=viridisc)
 plot(pc1, col=viridisc)
@@ -46,6 +48,7 @@ plot(pc1sd3, col=viridisc)
 plot(pc1sd7, col=viridisc)
 
 # stack all the standard deviation layers
+
 sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
 names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7")
 plot(sdstack, col=viridisc)
